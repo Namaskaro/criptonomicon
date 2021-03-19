9fdf21191ab3c0232c0407cb7a83d6e91765a66c
Namaskaro <45168111+Namaskaro@users.noreply.github.com>
2021-03-19 11:12:52 +0300

    Removed updateTicker from AddNewTicker.vue

--- src/components/AddNewTicker.vue
+++ src/components/AddNewTicker.vue
@@ -61,16 +61,7 @@ filteredCoins() {
   },
 methods: {
     ...mapActions(["getCoins","addTicker"]),
-    updateTicker(tickerName, price) {
-      this.tickers
-        .filter(t => t.name === tickerName)
-        .forEach(t => {
-          if (t === this.selectedTicker) {
-            this.graph.push(price);
-          }
-          t.price = price;
-        });
-    },
+   
 
     formatPrice(price) {
       if (price === "-") {
